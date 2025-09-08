153d7fc2cd99b15315db0708c31e748818c907cf
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-09-08 09:47:49 +0700

    364. Setting up the Structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { FaBeer } from "react-icons/fa";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline flex bg-black text-white h-screen justify-center items-center">
+        Hello world! <FaBeer />
+      </h1>
+    </>
+  );
+}
+
+export default App;
