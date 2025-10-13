84859c265a194c6fa6b75d2341eee01277869a52
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-13 15:15:00 +0700

    371. Fetching Products From the API

--- src/store/reducers/ProductReducer.js
+++ src/store/reducers/ProductReducer.js
@@ -4,4 +4,23 @@ const initialState = {
   pagination: {},
 };
 
-export const productReducer = (state = initialState, action) => {};
+export const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_PRODUCTS":
+      return {
+        ...state,
+        products: action.payload,
+        pagination: {
+          ...state.pagination,
+          pageNumber: action.pageNumber,
+          pageSize: action.pageSize,
+          totalElements: action.totalElements,
+          totalPages: action.totalPages,
+          lastPage: action.lastPage,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
