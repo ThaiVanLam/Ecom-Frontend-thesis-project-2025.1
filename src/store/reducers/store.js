0f38f49258971e3c43ab5db086672961420323d7
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-10-16 10:01:42 +0700

    375. Managing Loaders and Error Scenarios

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./ProductReducer";
+import { errorReducer } from "./errorReducer";
 
 export const store = configureStore({
-  reducer: { products: productReducer },
+  reducer: { products: productReducer, errors: errorReducer },
   preloadedState: {},
 });
 
