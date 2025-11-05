3631e685e0aa25aa6ae3abec6c48a629d6c96ff1
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-05 17:13:27 +0700

    update

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen py-12 bg-cover bg-center"
+      style={{ backgroundImage: "url('src/assets/contact page/image1.jpg')" }}
+    >
+      Contact
+    </div>
+  );
+}
+
+export default Contact;
--- src/store/action/index.js
+++ src/store/action/index.js
@@ -3,7 +3,9 @@ import api from "../../api/api";
 export const fetchProducts = (queryString) => async (dispatch) => {
   try {
     dispatch({ type: "IS_FETCHING" });
-    const { data } = await api.get(`/public/products?${queryString}`);
+    const { data } = await api.get(
+      `/product-manager/public/products?${queryString}`
+    );
 
     console.log(data);
 
@@ -28,7 +30,7 @@ export const fetchProducts = (queryString) => async (dispatch) => {
 export const fetchCategories = () => async (dispatch) => {
   try {
     dispatch({ type: "CATEGORY_LOADER" });
-    const { data } = await api.get(`/public/categories`);
+    const { data } = await api.get(`/product-manager/public/categories`);
 
     dispatch({
       type: "FETCH_CATEGORIES",
