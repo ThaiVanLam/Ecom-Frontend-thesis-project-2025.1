e02a01d66ed64193554c06570b80ecac219216a1
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-12 11:22:29 +0700

    395. Building Add to Cart Functionality

--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import ProductViewModal from "./ProductViewModal";
 import { truncateText } from "../../utils/truncateText";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../store/action";
+import toast from "react-hot-toast";
 
 function ProductCard({
   productId,
@@ -18,12 +21,17 @@ function ProductCard({
   const btnLoader = false;
   const [selectedViewProduct, setSelectedViewProduct] = useState("");
   const isAvailable = quantity && Number(quantity) > 0;
+  const dispatch = useDispatch();
 
   const handleProductView = (product) => {
     if (!about) {
       setSelectedViewProduct(product);
       setOpenProductViewModal(true);
     }
+  };
+
+  const addToCartHandler = (cartItems) => {
+    dispatch(addToCart(cartItems, 1, toast));
   };
 
   return (
@@ -89,7 +97,17 @@ function ProductCard({
 
             <button
               disabled={!isAvailable || btnLoader}
-              onClick={() => {}}
+              onClick={() => {
+                addToCartHandler({
+                  productId,
+                  productName,
+                  image,
+                  description,
+                  quantity,
+                  price,
+                  specialPrice,
+                });
+              }}
               className={`bg-blue-500 ${
                 isAvailable
                   ? "opacity-100 hover:bg-blue-600 cursor-pointer"
