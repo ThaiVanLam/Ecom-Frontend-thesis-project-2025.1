ab152f917efa2a7ab044065d609ba9d2fec43e4d
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-15 09:57:16 +0700

    408. Setting up Login Interface

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen py-12 bg-cover bg-center"
-      style={{ backgroundImage: "url('src/assets/contact page/image1.jpg')" }}
+      style={{ backgroundImage: "url('src/assets/contact page/image2.jpg')" }}
     >
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
         <h1 className="text-4xl font-bold text-center mb-6">Contact us</h1>
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineLogin } from "react-icons/ai";
+import InputField from "../shared/InputField";
+
+function Login() {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onTouched" });
+
+  const loginHandler = async (data) => {
+    console.log("login click");
+  };
+  return (
+    <div className="min-h-[calc(100vh-64px)] flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(loginHandler)}
+        className="sm:w-[450px] w-[360px] shadow-custom py-8 sm:px-8 px-4 rounded-md"
+      >
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <AiOutlineLogin className="text-slate-800 text-5xl" />
+          <h1 className="text-slate-800 text-center font-montserrat lg:text-3xl text-2xl font-bold">
+            Login Here
+          </h1>
+        </div>
+        <hr className="mt-2 mb-5 text-black" />
+        <div className="flex flex-col gap-3">
+          <InputField
+            label="UserName"
+            required
+            id="username"
+            type="text"
+            register={register}
+            message="*UserName is required"
+            placeholder="Enter your username"
+            errors={errors}
+          />
+          <InputField
+            label="Password"
+            required
+            id="password"
+            type="password"
+            register={register}
+            message="*Password is required"
+            placeholder="Enter your password"
+            errors={errors}
+          />
+        </div>
+        <button
+          disabled={loader}
+          className="bg-gradient-to-r from-indigo-500 to-emerald-500 flex gap-2 items-center justify-center font-semibold text-white w-full py-2 hover:from-indigo-400 hover:to-emerald-400 transition-colors duration-100 ease-in-out transform rounded-sm my-3 cursor-pointer rounded-md"
+          type="submit"
+        >
+          {loader ? <>Loading...</> : <>Login</>}
+        </button>
+
+        <p className="text-center text-sm text-slate-700 mt-6">
+          Don't have an account?
+          <Link
+            className="font-semibold underline hover:text-black"
+            to="/register"
+          >
+            <span> SignUp</span>
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
