6cc68a4a6684546d55f0f4a46e14c2da18c01b8b
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-27 12:18:19 +0700

    453. Create Payment Form in Stripe

--- src/components/checkout/StripePayment.jsx
+++ src/components/checkout/StripePayment.jsx
@@ -1,14 +1,23 @@
 import { Alert, AlertTitle } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
+import PaymentForm from "./PaymentForm";
 
 function StripePayment() {
+  const { clientSecret } = useSelector((state) => state.auth);
+  const { totalPrice } = useSelector((state) => state.carts);
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+  const { user, selectedUserCheckoutAddress } = useSelector(
+    (state) => state.auth
+  );
   return (
-    <div className="h-96 flex justify-center items-center">
-      <Alert severity="warning" variant="filled" style={{ maxWidth: "400px" }}>
-        <AlertTitle>Stripe Unavailable</AlertTitle>
-        Stripe payment is unavailable. Please use another payment method.
-      </Alert>
-    </div>
+    <>
+      {clientSecret && (
+        <Element stripe={stripePromise} options={{ clientSecret }}>
+          <PaymentForm clientSecret={clientSecret} totalPrice={totalPrice} />
+        </Element>
+      )}
+    </>
   );
 }
 
