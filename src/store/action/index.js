c0aef63e3e5bf386404a65885ec242db7f400f88
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-11-28 11:04:02 +0700

    457. Creating Order Confirmation Page

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -204,6 +204,8 @@ export const getUserAddresses = () => async (dispatch, getState) => {
 };
 
 export const selectUserCheckoutAddress = (address) => {
+  localStorage.setItem("CHECKOUT_ADDRESS", JSON.stringify(address));
+
   return {
     type: "SELECT_CHECKOUT_ADDRESS",
     payload: address,
@@ -294,5 +296,30 @@ export const createStripePaymentSecret =
       toast.error(
         error?.response?.data?.message || "Failed to create client secret"
       );
+    }
+  };
+
+export const stripePaymentConfirmation =
+  (sendData, setErrorMessage, setLoading, toast) =>
+  async (dispatch, getState) => {
+    try {
+      const { response } = await api.post(
+        "/order-manager/api/order/users/payments/online",
+        sendData
+      );
+      if (response.data) {
+        localStorage.removeItem("cartItems");
+        localStorage.removeItem("client-secret");
+        dispatch({ type: "REMOVE_CLIENT_SECRET_ADDRESS" });
+        dispatch({ type: "CLEAR_CART" });
+        toast.success("Order Accepted");
+      } else {
+        setErrorMessage("Payment Failed. Please try again.");
+      }
+
+      localStorage.setItem("client-secret", JSON.stringify(data));
+      dispatch({ type: "IS_SUCCESS" });
+    } catch (error) {
+      setErrorMessage("Payment Failed. Please try again.");
     }
   };
