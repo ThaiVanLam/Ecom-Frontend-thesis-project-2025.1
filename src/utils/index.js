05790a1981fd61e23e5565a29d285c01fdc30c79
Thái Văn Lâm <thaivanlam373@gmail.com>
2025-12-02 15:27:39 +0700

    475. Redux & Layout Setup for Admin Orders Section

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,12 @@
 import { href } from "react-router-dom";
 import { bannerImageOne, bannerImageThree, bannerImageTwo } from "./constant";
-import { FaBoxOpen, FaHome, FaStore, FaThList } from "react-icons/fa";
+import {
+  FaBoxOpen,
+  FaHome,
+  FaShoppingCart,
+  FaStore,
+  FaThList,
+} from "react-icons/fa";
 
 export const bannerLists = [
   {
@@ -30,6 +36,7 @@ export const bannerLists = [
 
 export const adminNavigation = [
   { name: "Dashboard", href: "/admin", icon: FaHome, current: true },
+  { name: "Orders", href: "/admin/orders", icon: FaShoppingCart },
   { name: "Products", href: "/admin/products", icon: FaBoxOpen, current: true },
   {
     name: "Categories",
